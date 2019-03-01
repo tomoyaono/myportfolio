5880491031733e66f34ca59cfd0e7f002062d5dc
EC2 Default User <ec2-user@ip-172-31-10-167.us-east-2.compute.internal>
2019-03-01 03:37:32 +0000

    cloudinary uploader ver2.1

--- app/uploaders/subimage_uploader.rb
+++ app/uploaders/subimage_uploader.rb
@@ -4,50 +4,44 @@ class SubimageUploader < CarrierWave::Uploader::Base
      include Cloudinary::CarrierWave
    else
      include CarrierWave::RMagick
-   end
-  
-  storage :file
-  # include CarrierWave::MiniMagick
+     
+     storage :file
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
+  #storage :file
   # storage :fog
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+    def store_dir
+      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
   
   # 画像の上限を700pxにする
-  process :resize_to_limit => [1000, 1000]
+    process :resize_to_limit => [1000, 1000]
 
   # 保存形式をJPGにする
-  process :convert => 'jpg'
+    process :convert => 'jpg'
 
   # サムネイルを生成する設定
-  version :thumb do
-    process :resize_to_fill => [300, 300]
-  end
+    version :thumb do
+      process :resize_to_fill => [300, 300]
+    end
 
   # jpg,jpeg,gif,pngしか受け付けない
-  def extension_white_list
-    %w(jpg jpeg gif png pdf)
-  end
-  
-  def public_id
-    return model.id 
-  end
+    def extension_white_list
+      %w(jpg jpeg gif png pdf)
+    end
 
  # 拡張子が同じでないとGIFをJPGとかにコンバートできないので、ファイル名を変更
-  def filename
-    super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
-  end
+    def filename
+      super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
+     end
  # 正方形のサムネを作る  
-  def create_square
-    manipulate! do |img|
-      narrow = img.columns > img.rows ? img.rows : img.columns
-      img.crop(Magick::CenterGravity, narrow, narrow).resize(size, size)
+    def create_square
+      manipulate! do |img|
+        narrow = img.columns > img.rows ? img.rows : img.columns
+        img.crop(Magick::CenterGravity, narrow, narrow).resize(size, size)
+      end
     end
-  end
+    
+   end
+
 end
